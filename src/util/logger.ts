94b88ce938e1e442c6a35546ee5e58dead37dc10
Nathan Dykstra <NathanDykstra@users.noreply.github.com>
2020-10-15 23:15:06 -0500

    add custom logger for typeorm; update winston logs to differentiate levels to transports better; log level can be set in .env file

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -1,5 +1,6 @@
 import winston from "winston";
 import dotenv from "dotenv";
+import { Logger, QueryRunner } from 'typeorm';
 
 dotenv.config();
 
@@ -7,11 +8,17 @@ dotenv.config();
  * Default logger.
  */
 export const logger = winston.createLogger({
-    level: 'info',
-    format: winston.format.json(),
     transports: [
-        new winston.transports.File({ filename: 'error.log', level: 'error' }),
-        new winston.transports.File({ filename: 'combined.log' }),
+        new winston.transports.File({
+            filename: 'larder_error.log',
+            level: 'error',
+            format: winston.format.combine(winston.format.logstash(), winston.format.timestamp())
+        }),
+        new winston.transports.File({
+            filename: 'larder.log',
+            level: process.env.LOG_LEVEL,
+            format: winston.format.combine(winston.format.logstash(), winston.format.timestamp())
+        })
     ],
 });
 
@@ -23,4 +30,47 @@ if (process.env.NODE_ENV !== 'production') {
         format: winston.format.simple(),
         level: 'debug'
     }));
+}
+
+/**
+ * Custom logger for use in typeorm.
+ */
+export class TypeOrmLogger implements Logger {
+
+    logQuery(query: string, parameters?: any[], _queryRunner?: QueryRunner) {
+        logger.info('typeorm query', {query, parameters});
+    }
+
+    logQueryError(error: string, query: string, parameters?: any[], _queryRunner?: QueryRunner) {
+        logger.error('typeorm error', {error, query, parameters});
+    }
+
+    logQuerySlow(time: number, query: string, parameters?: any[], _queryRunner?: QueryRunner) {
+        logger.warn('typeorm slow query', {time, query, parameters});
+    }
+
+    logSchemaBuild(message: string, _queryRunner?: QueryRunner) {
+        logger.info('typeorm schema build', {message});
+    }
+
+    logMigration(message: string, _queryRunner?: QueryRunner) {
+        logger.info('typeorm migration', {message});
+    }
+
+    log(level: 'info' | 'log' | 'warn', message: any, _queryRunner?: QueryRunner) {
+        switch (level) {
+            case 'info':
+                logger.info('typeorm log', {message});
+                break;
+            case 'log':
+                logger.debug('typeorm log', {message});
+                break;
+            case 'warn':
+                logger.warn('typeorm log', {message});
+                break;
+            default:
+                break;
+        }
+    }
+
 }
